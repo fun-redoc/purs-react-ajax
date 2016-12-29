56293b57caa2e61ae443448a8bfd30217836bfa1
Roland Stellmach <fun.redoc@gmail.com>
2016-12-29 18:34:05 +0000

    initial

--- test/Main.purs
+++ test/Main.purs
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-module Test.Main where
-
-import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
-
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "You should add some tests."
-=======
 module Test.Main (main) where
 
 import Prelude
@@ -55,4 +44,3 @@ main = runTest do
 --      equal (snd result) false 
 --      equal (fst result) input 
 --      assertFalse "state and expected word should be different" ((fst result) == expected )
->>>>>>> version
